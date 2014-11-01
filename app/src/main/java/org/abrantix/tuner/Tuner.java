0f2c48bc0e05ddb9ab92fc6270834e7ed12db117
Filipe Abrantes <filipe.abrantes@gmail.com>
2014-11-01 15:26:47 +0000

    initial project

---
+++ app/src/main/java/org/abrantix/tuner/Tuner.java
@@ -0,0 +1,27 @@
+package org.abrantix.tuner;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.wearable.view.WatchViewStub;
+import android.util.Log;
+import android.widget.TextView;
+
+public class Tuner extends Activity {
+
+    private static final String TAG = "Tuner";
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tuner);
+        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+            @Override
+            public void onLayoutInflated(WatchViewStub stub) {
+                mTextView = (TextView) stub.findViewById(R.id.text);
+                Log.d(TAG, "TextView: " + mTextView.getText() + " view=" + mTextView);
+            }
+        });
+    }
+}
